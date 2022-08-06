1c616bf2097c4880bd0a2d5918c140db9af1190b
dilekbakar <dilekbakar22@gmail.com>
2022-08-06 22:18:47 +0300

    commit
    create, drop

---
+++ sorgular.sql
@@ -0,0 +1,17 @@
+-- create table urun(
+-- id integer primary key not null,
+-- ad varchar(15) not null,
+-- marka varchar(15),
+-- stok integer,
+-- kategori varchar(15)
+-- ) 
+
+-- create table kategori(
+-- id integer not null primary key,
+-- 	ad varchar(15) not null
+-- )
+
+-- create table tablo (
+-- deneme varchar)
+
+-- drop table tablo
