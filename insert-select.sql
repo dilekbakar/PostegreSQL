e0843a09027fae26cba6fd27a5928cecd7b93149
dilekbakar <dilekbakar22@gmail.com>
2022-08-06 22:36:35 +0300

    commit
    insert-select

---
+++ insert-select.sql
@@ -0,0 +1,19 @@
+-- insert into musteri(id,ad,soyad,sehir) values (5,'Elif','Hüner','Antalya');
+-- insert into musteri(id,ad,soyad,sehir) values (6,'Ahmet','Yıldırım','Ankara');
+-- insert into musteri(id,ad,soyad,sehir) values (7,'Tunç','Atak','Muğla');
+-- insert into musteri(id,ad,soyad,sehir) values (8,'Tufan','Güney','Bursa');
+
+--select id,ad,soyad from musteri
+
+--select sehir from musteri
+
+--select * from musteri
+--select * from musteri where sehir = 'Ankara'
+
+--select ad from musteri where id < 5
+
+--select * from musteri where id < 5 and sehir = 'Ankara' or sehir = 'Muğla'
+
+--select * from musteri order by id
+
+--select * from musteri order by ad ASC 
