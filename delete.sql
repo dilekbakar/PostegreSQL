7e2fa4c4d565b30d80d7166eb02e740116f9ea2a
dilekbakar <dilekbakar22@gmail.com>
2022-08-07 00:23:17 +0300

    commit
    update - delete

---
+++ delete.sql
@@ -0,0 +1,11 @@
+--INSERT
+--SELECT
+--DELETE
+
+--DELETE from musteri where id = 8
+
+--select * from musteri
+
+--DElETE from musteri where ad ='Veli'
+
+--select * from musteri
---
+++ update.sql
@@ -0,0 +1,14 @@
+--UPDATE
+
+update musteri set bakiye = 6780 where id = 1
+
+select * from musteri
+
+select * from musteri order by id
+
+update musteri set sehir = 'Rize' where sehir = 'Ankara'
+
+select * from musteri order by id
+
+update musteri set sehir = 'Ankara' where sehir = 'Rize'
+
