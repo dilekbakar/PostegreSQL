0ace072f442fafd30f2c0dd43b5f46709e30b224
dilekbakar <dilekbakar22@gmail.com>
2022-08-07 12:55:09 +0300

    commit
    subquery

--- subquery.sql
+++ subquery.sql
@@ -18,3 +18,9 @@ insert into meslek (id,ad) values(3,'mühendis');
 
 select * from musteri
 
+select * from musteri where meslek = (select id from meslek where ad = 'öğretmen')
+
+
+update musteri set bakiye = bakiye + bakiye*0.1 where meslek = (select id from meslek where ad = 'mühendis')
+
+select * from musteri
